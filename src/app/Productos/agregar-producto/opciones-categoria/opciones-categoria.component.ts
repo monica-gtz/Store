f8d06ad6f07197e8fa39e8adca26a1a697069999
Monica Gutierrez <monica.gtzm@hotmail.com>
2022-08-09 19:07:15 -0500

    add dual-list

--- src/app/Productos/agregar-producto/opciones-categoria/opciones-categoria.component.ts
+++ src/app/Productos/agregar-producto/opciones-categoria/opciones-categoria.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CategoriaService } from '../../../services/categoria.service';
 import { ProductoService } from '../../../services/producto.service';
 import { AddProductView, AddCategorieView } from '../../../Models/Models';
+import { DualListComponent } from '../../../dual-list/dual-list.component';
 
 @Component({
   selector: 'app-opciones-categoria',
@@ -10,14 +11,23 @@ import { AddProductView, AddCategorieView } from '../../../Models/Models';
 })
 export class OpcionesCategoriaComponent implements OnInit {
 
-  public listaCategorias: AddCategorieView[] = [];
+  private listaCategorias: AddCategorieView[];
+  tab = 1;
+  keepSorted = true;
+  display: string;
+  filter = false;
+  source: AddCategorieView[];
+  confirmed: AddCategorieView[];
+  userAdd = '';
+  disabled = false;
+
+  sourceLeft = true;
+  format: any = DualListComponent.DEFAULT_FORMAT;
+ 
 
   constructor(private categoriaService : CategoriaService) { }
 
   ngOnInit(): void {
     this.categoriaService.getAllCategories().subscribe();
   }
-
-  source = this.listaCategorias;
-	target = [this.listaCategorias];
 }
